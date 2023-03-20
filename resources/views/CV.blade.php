ff183858bfecb5cd023ae6bc91019589c4c9f50d
Davina <davina101204@gmail.com>
2023-03-20 18:15:14 +0700

    project cv init

---
+++ resources/views/CV.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>CV-Davina-2231051</title>
+</head>
+
+<body>
+    <div>
+    <h1 align="center"><u>Curriculum Vitae</u></h1>
+    <tab>
+    <img src="img/foto.jpeg" alt="Davina">
+    <br>
+    <p><b>Data Pribadi</b></p>
+     <p>
+     Nama                    : Davina
+    <br>
+     Tempat/Tanggal Lahir    : Batam, 10 Desember 2004
+    <br>
+    Nomor Handphone         : 085836001878
+     <br>
+     Jenis Kelamin           : Perempuan
+     <br>
+     Agama                   : Kristen Protestan
+     <br>
+     Kewarganegaraan         : Indonesia
+     <br>
+     Status                  : Belum Menikah
+     <br>
+    Email                   : davina101204@gmail.com
+    </p>
+
+    <p><b>Pendidikan Formal</b></p>
+    <table>
+        <tr>
+            <th>Periode</th>
+            <th>Sekolah</th>
+            <th>Jurusan</th>
+        </tr>
+        <tr>
+            <td>2016-2019</td>
+            <td>SMP Kristen Tabgha</td>
+            <td>-</td>
+        </tr>
+        <tr>
+            <td>2019-2022</td>
+            <td>SMA Cerdas Mandiri</td>
+            <td>IPA</td>
+        </tr>
+        <tr>
+            <td>2022-sekarang</td>
+            <td>Universitas Internasional Batam</td>
+            <td>Sistem Informasi</td>
+        </tr>
+
+    </div></tab>
+</body>
+</html>
